39ac5f47e367497d31a861ca4aedb1e58de41005
Drew Rogers <regdogg.acr@gmail.com>
2025-02-13 22:32:51 -0600

    Add a local multiplayer gameserver entry option. ; Code Cleanup.

--- toontown/toonbase/OpeningUserInput.py
+++ toontown/toonbase/OpeningUserInput.py
@@ -1,9 +1,21 @@
 import builtins
+import re
 from direct.showbase.DirectObject import DirectObject
 from direct.gui.DirectGui import *
 from otp.otpbase import OTPGlobals
 from otp.otpgui import OTPDialog
-from otp.otpbase.OTPLocalizer import CREnterUsername, CRInvalidUsername, CREmptyUsername, CRLoadingGameServices, CRSpecifyServerSelection, CRSingleplayer, CRPublicServer
+from otp.otpbase.OTPLocalizer import (
+    CREnterUsername,
+    CRInvalidUsername,
+    CREmptyUsername,
+    CREnterGameserver,
+    CREmptyGameserver,
+    CRLoadingGameServices,
+    CRSpecifyServerSelection,
+    CRSingleplayer,
+    CRLocalMultiplayer,
+    CRPublicServer
+)
 
 class OpeningUserInput(DirectObject):
     def __init__(self, cr, launcher):
@@ -20,7 +32,7 @@ class OpeningUserInput(DirectObject):
     def clearText(self):
         self.entry.enterText('')
 
-    def determineAuthenticity(self, textEntered):
+    def determineUsernameAuthenticity(self, textEntered):
         username = textEntered
         if not username:
             self.askForUsername['text'] = CREmptyUsername
@@ -30,42 +42,65 @@ class OpeningUserInput(DirectObject):
             self.cr.playToken = username
             self.askServerPreference()
 
+    def specifyGameserver(self, textEntered):
+        gameserver = textEntered
+        if not gameserver:
+            base.startShow(self.cr, '127.0.0.1:7198')
+        else:
+            base.startShow(self.cr, gameserver)
+
     def askForPlaytoken(self):
         self.askForUsername = self.dialogClass(message=CREnterUsername, style=OTPDialog.NoButtons,
                                      doneEvent='cleanup', text_wordwrap=16, midPad=0.2, extraArgs=['askForUsername'])
         self.accept('cleanup', self.cleanup, extraArgs=[self.askForUsername])
 
-        self.entry = DirectEntry(parent=self.askForUsername , text="", scale=0.0625, pos=(-0.3, 0, -0.19), command=self.determineAuthenticity,
-                            cursorKeys=1, obscured=1, initialText="Username", numLines=1, focus=1, focusInCommand=self.clearText)
+        self.entry = DirectEntry(parent=self.askForUsername , text="", scale=0.0625, pos=(-0.3, 0, -0.19), command=self.determineUsernameAuthenticity,
+                                 cursorKeys=1, obscured=1, initialText="Username", numLines=1, focus=1, focusInCommand=self.clearText)
 
         self.askForUsername.show()
-        self.accept('determineAuthenticity', self.determineAuthenticity)
+
+    def localMultiplayerScreen(self):
+        self.askForGameserver = self.dialogClass(message=CREnterGameserver, style=OTPDialog.NoButtons,
+                                     doneEvent='cleanup', text_wordwrap=20, midPad=0.2, extraArgs=['askForGameserver'])
+        self.accept('cleanup', self.cleanup, extraArgs=[self.askForGameserver])
+
+        self.entry = DirectEntry(parent=self.askForGameserver , text="", scale=0.075, pos=(-0.6, 0, -0.3), command=self.specifyGameserver,
+                                 width=16, cursorKeys=1, obscured=0, initialText="Gameserver", numLines=1, focus=1, focusInCommand=self.clearText)
+
+        self.askForGameserver.show()
+
+    def publicServerScreen(self):
+        base.startShow(self.cr, config.ConfigVariableString('public-server-ip', '').getValue())
+
+    def singlePlayerScreen(self):
+        # Start DedicatedServer
+        builtins.gameServicesDialog = self.dialogClass(message=CRLoadingGameServices)
+        builtins.gameServicesDialog.show()
+
+        from toontown.toonbase.DedicatedServer import DedicatedServer
+        builtins.clientServer = DedicatedServer(localServer=True)
+        builtins.clientServer.start()
+
+        def localServerReady():
+            builtins.gameServicesDialog.cleanup()
+            del builtins.gameServicesDialog
+            base.startShow(self.cr)
+
+        self.accept('localServerReady', localServerReady)
 
     def decision(self, buttonValue = None):
-        if buttonValue == -1: # buttonValue returning -1 will connect us to the public seever.
-            base.startShow(self.cr, config.ConfigVariableString('public-server-ip', '').getValue())
-        else: # buttonValue returning any other value will startup the Singleplayer server.
-            # Start DedicatedServer
-            builtins.gameServicesDialog = self.dialogClass(message = CRLoadingGameServices)
-            builtins.gameServicesDialog.show()
-
-            from toontown.toonbase.DedicatedServer import DedicatedServer
-            builtins.clientServer = DedicatedServer(localServer=True)
-            builtins.clientServer.start()
-
-            def localServerReady():
-                builtins.gameServicesDialog.cleanup()
-                del builtins.gameServicesDialog
-                base.startShow(self.cr)
-            self.accept('localServerReady', localServerReady)
+        if buttonValue == -1: # buttonValue returning -1 will connect us to the Local Multiplayer server.
+            self.localMultiplayerScreen()
+        elif buttonValue == 0: # buttonValue returning 0 will connect us to the public server.
+            self.publicServerScreen()
+        elif buttonValue == 1: # buttonValue returning 1 will startup the Singleplayer server.
+            self.singlePlayerScreen()
 
     def askServerPreference(self):
         if not config.ConfigVariableBool('local-multiplayer', False).getValue():
-            from otp.otpgui import OTPDialog
-
-            self.askServerSpecification = self.dialogClass(message=CRSpecifyServerSelection, style=OTPDialog.TwoChoiceCustom,
-                                                 okButtonText=CRSingleplayer, cancelButtonText=CRPublicServer, command=self.decision,
-                                                 doneEvent='cleanup', text_wordwrap=16, buttonPadSF=5)
+            self.askServerSpecification = self.dialogClass(message=CRSpecifyServerSelection, style=OTPDialog.ThreeChoiceCustom,
+                                                           yesButtonText=CRSingleplayer, noButtonText=CRPublicServer, cancelButtonText=CRLocalMultiplayer,
+                                                           command=self.decision, doneEvent='cleanup', text_wordwrap=20, buttonPadSF=5)
             self.askServerSpecification.show()
             self.accept('cleanup', self.cleanup, extraArgs=[self.askServerSpecification])
         else:
